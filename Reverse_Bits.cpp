88849b916bfa7b0ba50144c22720b77e87a03a5d
AdiiStaR <117852975+AdiiStaR@users.noreply.github.com>
2022-11-11 00:03:53 +0530

    Create Reverse_Bits.cpp

---
+++ Reverse_Bits.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long X) {
+        long long reverse =0;
+        
+        int n=1;
+        while(n<=32){
+           int remain = X%2;
+           reverse = reverse*2+remain;
+           X/=2;
+           n++;
+        }
+        return reverse;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
