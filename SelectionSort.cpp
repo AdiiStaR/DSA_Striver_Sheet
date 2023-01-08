0180f79ed385d340d343242d2f3af491ffb3eba8
ADITYA SHIROMANI <117852975+AdiiStaR@users.noreply.github.com>
2023-01-09 04:14:41 +0530

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -67,3 +67,35 @@ int main()
 }
 
 // } Driver Code Ends
+//Another Code
+
+class Solution
+{
+    public:
+  
+    int select(int arr[], int i, int n)
+    {
+        // code here such that selectionSort() sorts arr[]
+        int min = i;
+        for(int j =i+1;j<n;j++){
+            if(arr[j]<arr[min]){
+                min = j;
+            }
+        }
+        return min;
+    }
+     
+    void selectionSort(int arr[], int n)
+    {
+       //code here
+      
+      
+       for(int i =0;i<n-1;i++){
+           
+            int min_val = select(arr,i,n);
+               swap(arr[min_val],arr[i]);
+           
+       }
+       
+    }
+};
